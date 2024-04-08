b1cb106e30e081cff32b5e1e210d90ac692d72f7
zainraza2000 <zainraza20000@gmail.com>
2024-04-08 18:37:55 +0500

    init

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -7,16 +7,37 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3330;
+  const USERS_SERVICE_URL = "http://localhost:3334";
+  const TEACHER_SERVICE_URL = "http://localhost:3332";
+  const COURSE_SERVICE_URL = "http://localhost:3333";
+
+
+  // Proxy endpoints
+  app.use('/api/users', createProxyMiddleware({
+    target: USERS_SERVICE_URL,
+    changeOrigin: true,
+  }));
+  app.use('/api/teachers', createProxyMiddleware({
+    target: TEACHER_SERVICE_URL,
+    changeOrigin: true,
+  }));
+  app.use('/api/courses', createProxyMiddleware({
+    target: COURSE_SERVICE_URL,
+    changeOrigin: true,
+  }));
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+
 }
 
 bootstrap();
---
+++ apps/course/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'course',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/course',
+};
